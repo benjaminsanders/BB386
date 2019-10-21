2efaec47b726d94ec2bfddb7e0ab0c8efbc6d2ab
Benjamin Sanders <contact@benjaminsanders.net>
2019-10-20 20:27:44 -0400

    Updated names and index

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>BB386</title>
-</head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
+    <title>Benjamin Sanders</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/css/_386.css" rel="stylesheet">
+    <link href="./assets/css/_386_alt.css" rel="stylesheet">
+	   <!-- jQuery Script -->
+	<script src="./assets/js/jquery.js"></script>
+	  
+	<script>
+	 $(function () {
+	  $('[data-toggle="tooltip"]').tooltip()
+	 });
+	</script>
+	  
+  </head>
+<body>
+	<div class="container">
+	
+	</div>
 <body>
 </body>
 </html>
